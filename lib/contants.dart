bc896c9c80c456422f822d43901910ba90c1d762
Suhairayousuf <suhairaasif42@gmail.com>
2023-10-16 13:13:26 +0530

    initial commit

---
+++ lib/contants.dart
@@ -0,0 +1,5 @@
+class MyConst {
+  // add your appId and appSign from console
+  static const int appId = 12270530;
+  static const String appSign = "856b744b4b5f2d008b93f49e688c3fd5fb3fabfa2568f9106f7a20520f323ac6";
+}
